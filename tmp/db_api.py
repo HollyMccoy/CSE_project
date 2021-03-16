275ca7de27a33a3c2290d49324a501f2ba2ecb86
Aidan <48849078+Ouras@users.noreply.github.com>
2021-03-16 17:12:58 -0400

    Add files via upload

---
+++ tmp/db_api.py
@@ -0,0 +1,21 @@
+import requests
+import datetime
+
+def post_db(sound,device):
+    #sound class variable pass into function
+    sound_class=sound
+    #deviceID will also pass into function
+    deviceID=device
+    
+    URL = "https://rvx6tnrod3.execute-api.us-east-2.amazonaws.com/beta/classification"
+    headers = {"Content-Type":"aplication/json"}
+    params = {"payload":"payload???"}
+
+    data = dict(deviceID=deviceID,date=str(datetime.date.today().strftime("%m/%d/%y")),UTC=str(datetime.datetime.now().strftime("%H:%M:%S")),soundID=sound_class)
+
+    response = requests.post(URL,json=data,headers= headers)
+
+    print(response.text)
+
+
+post_db("cat","aidan")
---
+++ tmp/test.py
@@ -0,0 +1,3 @@
+from record import record
+
+record(seconds=12, out="232323/out.wav")
