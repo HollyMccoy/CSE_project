feca4c4db19de6a09b3b8ba62fd8a52dc0a403be
Holly <55715908+HollyMccoy@users.noreply.github.com>
2021-03-18 14:47:13 -0400

    using wavio instead

--- record.py
+++ record.py
@@ -1,12 +1,24 @@
 import sounddevice as sd
+import numpy as np
 from scipy.io.wavfile import write
+import wavio
 
-def record(seconds=20, out="out.wav", sample_rate=16000, channels=1):
 
-    myrecording = sd.rec(int(seconds * sample_rate), samplerate=sample_rate, channels=1)
-    sd.wait()  # Wait until recording is finished
-    write(out, sample_rate, myrecording)  # Save as WAV file
-    print(out)
+def record(seconds=20, out="out.wav", sample_rate=16000, channels=1):  #
+    #
+    myrecording = sd.rec(int(seconds * sample_rate), samplerate=sample_rate, channels=1)  #
+    sd.wait()  # Wait until recording is finished#
+    # write(out, sample_rate, myrecording)  # Save as WAV file#
+
+
+    T = 3  # sample duration (seconds)
+    f = 441.0  # sound frequency (Hz)#
+    t = np.linspace(0, T, T * sample_rate, endpoint=False)#
+    x = np.sin(2 * np.pi * f * t)#
+#
+    wavio.write(out,x, sample_rate, sampwidth=2)#
+
+    # print(out)
 ### import subprocess
 ### import os
 ###
