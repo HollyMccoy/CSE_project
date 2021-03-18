61dea89cb5da82a19bf72aca73e7c83e74e31254
Holly <55715908+HollyMccoy@users.noreply.github.com>
2021-03-18 16:31:43 -0400

    Update record.py

--- record.py
+++ record.py
@@ -4,21 +4,17 @@ from scipy.io.wavfile import write
 import wavio
 
 
-def record(seconds=20, out="out.wav", sample_rate=16000, channels=1):  #
+def record(seconds=20, out="out.wav", sample_rate=16000, channels=2):  #
     #
-    myrecording = sd.rec(int(seconds * sample_rate), samplerate=sample_rate, channels=1)  #
+    myrecording = sd.rec(int(seconds * sample_rate), samplerate=sample_rate, channels=2)  #
     sd.wait()  # Wait until recording is finished#
     # write(out, sample_rate, myrecording)  # Save as WAV file#
 
-
-    T = 20  # sample duration (seconds)
-    f = 16000  # sound frequency (Hz)#
-    t = np.linspace(0, T, T * sample_rate, endpoint=False)#
-    x = np.sin(2 * np.pi * f * t)#
-#
     wavio.write(out,myrecording, sample_rate, sampwidth=2)#
 
     # print(out)
+
+
 ### import subprocess
 ### import os
 ###
