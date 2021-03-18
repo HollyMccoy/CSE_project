c76c94d479db8f3c4c146c34849fda4cfc3e33cd
Holly <55715908+HollyMccoy@users.noreply.github.com>
2021-03-18 13:48:03 -0400

    new recording version
    python3 -m pip install sounddevice
    sudo apt-get install libsound-dev
    sudo apt-get install libportadio2
    sudo apt-get install libsound-dev portaudio19-dev libportadio2 libportaudiocpp0
    sudo pip install pyaudio

--- record.py
+++ record.py
@@ -1,17 +1,27 @@
-import subprocess
-import os
+import sounddevice as sd
+from scipy.io.wavfile import write
 
-def record(seconds=0, out="out.wav", sample_rate=44100, channels=2):
-    proc_args = ["arecord", "-D", "hw:1,0", "-f", "S16_LE", "-r", str(sample_rate), "-c", str(channels), out]
+def record(seconds=20, out="out.wav", sample_rate=16000, channels=1):
 
-    if (seconds > 0):
-        proc_args.append("-d")
-        proc_args.append(str(seconds))
-
-    proc = subprocess.Popen(proc_args, shell=False, preexec_fn=os.setsid)
-    
-    print("Starting recording... pid =" + str(proc.pid))
-
-    if (seconds > 0):
-        proc.wait()
-        print("Recording done")
+    myrecording = sd.rec(int(seconds * sample_rate), samplerate=sample_rate, channels=1)
+    sd.wait()  # Wait until recording is finished
+    write(out, sample_rate, myrecording)  # Save as WAV file
+    print(out)
+### import subprocess
+### import os
+###
+###
+### def record(seconds=0, out="out.wav", sample_rate=16000, channels=2):
+###     proc_args = ["arecord", "-D", "hw:1,0", "-f", "S16_LE", "-r", str(sample_rate), "-c", str(channels), out]
+###
+###     if (seconds > 0):
+###         proc_args.append("-d")
+###         proc_args.append(str(seconds))
+###
+###     proc = subprocess.Popen(proc_args, shell=False, preexec_fn=os.setsid)
+###
+###     print("Starting recording... pid =" + str(proc.pid))
+###
+###     if (seconds > 0):
+###         proc.wait()
+###         print("Recording done")
