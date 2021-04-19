bbc5b55319bf68e92e8ffbbd34a564651c9fac61
Holly <55715908+HollyMccoy@users.noreply.github.com>
2021-04-19 18:10:33 -0400

    adding roomID to posts

--- db_api.py
+++ db_api.py
@@ -11,11 +11,11 @@ def post_db(sound,device):
     headers = {"Content-Type":"aplication/json"}
     params = {"payload":"payload???"}
 
-    data = dict(deviceID=deviceID,date=str(datetime.date.today().strftime("%m/%d/%y")),UTC=str(datetime.datetime.now().strftime("%H:%M:%S")),soundID=sound_class)
+    data = dict(deviceID=deviceID,date=str(datetime.date.today().strftime("%m/%d/%y")),UTC=str(datetime.datetime.now().strftime("%H:%M:%S")),soundID=sound_class,roomID=#this device room id (for now the name of the nano holder))
 
     response = requests.post(URL,json=data,headers= headers)
 
     print(response.text)
 
 
-post_db("cat","holly")
+
