74a6c60f3f387b89be5fe7d0fd7460a49b6a42f9
Holly <55715908+HollyMccoy@users.noreply.github.com>
2021-04-20 17:43:28 -0400

    edit for record UTC instead of classify

--- db_api.py
+++ db_api.py
@@ -1,7 +1,7 @@
 import requests
 import datetime
 
-def post_db(sound,device):
+def post_db(sound,device,UTC):
     #sound class variable pass into function
     sound_class=sound
     #deviceID will also pass into function
@@ -11,7 +11,7 @@ def post_db(sound,device):
     headers = {"Content-Type":"aplication/json"}
     params = {"payload":"payload???"}
 
-    data = dict(deviceID=deviceID,date=str(datetime.date.today().strftime("%m/%d/%y")),UTC=str(datetime.datetime.now().strftime("%H:%M:%S")),soundID=sound_class)
+    data = dict(deviceID=deviceID,date=str(datetime.date.today().strftime("%m/%d/%y")),UTC=UTC,soundID=sound_class)
 
     response = requests.post(URL,json=data,headers= headers)
 
