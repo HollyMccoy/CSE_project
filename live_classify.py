a849ab4d45da1bd18f5a0a8a29e9f1dd851c3d48
Marian Antonio <73906482+marian-antonio@users.noreply.github.com>
2021-04-21 20:10:48 -0400

    Update live_classify.py

--- live_classify.py
+++ live_classify.py
@@ -53,10 +53,14 @@ def make_classification(args, src_dir, timestamp):
     y_pred = np.argmax(y_mean)#
     time_stamp = timestamp#
     print('-Predicted class: {}'.format(classes[y_pred]))#
+    end_time = datetime.datetime.now()
+    diff = end_time - timestamp
+    total_time = diff.total_seconds()
+    print(f"-Classification time: {round(total_time,2)} seconds")
     
     # make post request
     start_post = str(datetime.datetime.now().strftime("%H:%M:%S"))
-    print(f"Posting results to database. Start time: {start_post}")
+    print(f"-Posting results to database. Start time: {start_post}")
     post_db(classes[y_pred], "DEVICE ID", timestamp)   # replace "DEVICE ID" with desired name
 
 
@@ -96,10 +100,7 @@ if __name__ == '__main__':
         
         start_time = datetime.datetime.now()
         make_classification(args, output, start_time, date)
-        end_time = datetime.datetime.now()
-        diff = end_time - start_time
-        total_time = diff.total_seconds()
-        print(f"-Classification time: {round(total_time,2)} seconds")
+
         
         # 3. delete directory
         
