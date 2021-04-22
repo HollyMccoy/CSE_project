f48a34af43740cb2a13befa7b394d5de62889179
Marian Antonio <73906482+marian-antonio@users.noreply.github.com>
2021-04-21 20:01:18 -0400

    Update db_api.py

--- db_api.py
+++ db_api.py
@@ -1,22 +1,20 @@
 import requests
 import datetime
 
-def post_db(sound,device, d, t):
+def post_db(sound,device, t):
     #sound class variable pass into function
     sound_class=sound
     #deviceID will also pass into function
     deviceID=device
-    date_today = d
+    #time passed into function
     time_now = t
 
-    URL = "https://wb44syzvc9.execute-api.us-east-2.amazonaws.com/beta/sound"
+    URL = "https://wb44syzvc9.execute-api.us-east-2.amazonaws.com/beta/sound" # {insert API Gateway URL for triggering Lambda here}
     headers = {"Content-Type":"aplication/json"}
     params = {"payload":"payload???"}
 
-    data = dict(deviceID=deviceID,date=str(datetime.date.today().strftime("%m/%d/%y")),UTC=str(datetime.datetime.now().strftime("%H:%M:%S")),soundID=sound_class)
+    data = dict(deviceID=deviceID,date=str(datetime.date.today().strftime("%m/%d/%y")),UTC=time_now,soundID=sound_class)
     
-    #data = dict(deviceID=deviceID,date=date_today,UTC=time_now,soundID=sound_class)
-
     response = requests.post(URL,json=data,headers= headers)
 
     print(response.text)
